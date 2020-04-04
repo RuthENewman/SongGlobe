aa942762a1614c0d531c506d0ddfd867df83b067
Ruth Newman <ruthnewman@gmail.com>
2020-04-04 23:56:51 +0100

    amended sign up and login

---
+++ login.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Song Globe - sign up</title>
+</head>
+<body>
+    <div id="input-container">
+        <form action="login.php" 
+              method="POST" 
+              id="login-form">
+            <h2>Login</h2>
+            <p><label for="login-email">Email address: </label>
+                <input 
+                   id="login-email" 
+                   name="login-email" 
+                   type="email" 
+                   placeholder="Your email address" 
+                   required ></p>
+            <p>
+                <label for="login-password">Password: </label>
+                <input 
+                   id="login-password" 
+                   name="login-password" 
+                   type="password" 
+                   required >
+            </p>
+            <button type="submit" name="login-button">Log in</button>
+        </form>
+
+        <form action="signup.php" 
+              method="POST" 
+              id="signup-form">
+            <h2>Create an account</h2>
+            <p>
+                <label for="signup-firstname">First name: </label>
+                <input 
+                   id="signup-firstname" 
+                   name="signup-firstname" 
+                   type="text" 
+                   placeholder="Your first name"
+                   required >
+            </p>
+            <p>
+                <label for="signup-lastname">Last name: </label>
+                <input 
+                   id="signup-lastname" 
+                   name="signup-lastname" 
+                   type="text"
+                   placeholder="Your last name"
+                   required >
+            </p>
+            <p>
+                <label for="signup-email">Email address: </label>
+                <input 
+                   id="signup-email" 
+                   name="signup-email" 
+                   type="email" 
+                   placeholder="Your email address" 
+                   required >
+            </p>
+            <p>
+                <label for="signup-email--confirm">Confirm your email address: </label>
+                <input 
+                   id="signup-email--confirm" 
+                   name="signup-email--confirm" 
+                   type="email" 
+                   placeholder="Your email address" 
+                   required >
+            </p>
+            <p>
+                <label for="signup-password">Password: </label>
+                <input 
+                   id="signup-password" 
+                   name="signup-password" 
+                   type="password" 
+                   required >
+            </p>
+            <p>
+                <label for="signup-password--confirm">Confirm your password: </label>
+                <input 
+                   id="signup-password--confirm" 
+                   name="signup-password--confirm" 
+                   type="password" 
+                   required >
+            </p>
+            <button type="submit" name="signup-button">Sign up</button>
+        </form>
+    </div>
+</body>
+</html>
