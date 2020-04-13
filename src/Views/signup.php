391dc67ffe5a82878a858218bab8244b5baf56ee
Ruth Newman <ruthnewman@gmail.com>
2020-04-14 00:45:39 +0100

    more styling

--- src/Views/signup.php
+++ src/Views/signup.php
@@ -26,103 +26,104 @@
 </head>
 <body>
     <div id="background-image">
-        <div id="input-container">
-            <form action="signup.php" 
-                method="POST" 
-                id="login-form">
-                <h2>Login</h2>
-                <p><label for="login-email">Email address: </label>
-                    <span class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$loginFailed)); ?></span>
-                    <input 
-                    id="login-email" 
-                    name="login-email" 
-                    value="<?php getFormValue('login-email'); ?>"
-                    type="email" 
-                    placeholder="Your email address" 
-                    required ></p>
-                <p>
-                    <label for="login-password">Password: </label>
-                    <input 
-                    id="login-password" 
-                    name="login-password" 
-                    type="password" 
-                    placeholder="Your password"
-                    required >
-                </p>
-                <button type="submit" name="login-button">Log in</button>
-            </form>
-
-            <form action="signup.php" 
-                method="POST" 
-                id="signup-form">
-                <h2>Create an account</h2>
-                <p>
-                    <label for="signup-firstname">First name: </label>
-                    <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$firstNameLengthInvalid)); ?></p>
-                    <input 
-                    id="signup-firstname" 
-                    name="signup-firstname"
-                    value="<?php getFormValue('signup-firstname'); ?>"
-                    type="text" 
-                    placeholder="Your first name"
-                    required >
-                </p>
-                <p>
-                    <label for="signup-lastname">Last name: </label>
-                    <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$lastNameLengthInvalid)); ?></p>
-                    <input 
-                    id="signup-lastname" 
-                    name="signup-lastname" 
-                    value="<?php getFormValue('signup-lastname'); ?>"
-                    type="text"
-                    placeholder="Your last name"
-                    required >
-                </p>
-                <p>
-                    <label for="signup-email">Email address: </label>
-                    <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$emailInvalid)); ?></p>
-                    <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$emailAlreadyInUse)); ?></p>
-                    <input 
-                    id="signup-email" 
-                    name="signup-email" 
-                    value="<?php getFormValue('signup-email'); ?>"
-                    type="email" 
-                    placeholder="Your email address" 
-                    required >
-                </p>
-                <p>
-                    <label for="signup-email--confirm">Confirm email address: </label>
-                    <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$emailsDoNotMatch)); ?></p>
-                    <input 
-                    id="signup-email--confirm" 
-                    name="signup-email--confirm"
-                    value="<?php getFormValue('signup-email--confirm'); ?>" 
-                    type="email" 
-                    placeholder="Your email address" 
-                    required >
-                </p>
-                <p>
-                    <label for="signup-password">Password: </label>
-                    <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$passwordLengthInvalid)); ?></p>
-                    <input 
-                    id="signup-password" 
-                    name="signup-password" 
-                    type="password" 
-                    placeholder="Your password"
-                    required >
-                </p>
-                <p>
-                    <label for="signup-password--confirm">Confirm password: </label>
-                    <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$passwordsDoNotMatch)); ?></p>
-                    <input 
-                    id="signup-password--confirm" 
-                    name="signup-password--confirm" 
-                    type="password" 
-                    placeholder="Your password"
-                    required >
-                </p>
-                <button type="submit" name="signup-button">Sign up</button>
-            </form>
+        <div id ="login-container">
+            <div id="input-container">
+                <form action="signup.php" 
+                    method="POST" 
+                    id="login-form">
+                    <h2>Login</h2>
+                    <p><label for="login-email">Email address: </label>
+                        <span class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$loginFailed)); ?></span>
+                        <input 
+                        id="login-email" 
+                        name="login-email" 
+                        value="<?php getFormValue('login-email'); ?>"
+                        type="email" 
+                        placeholder="Your email address" 
+                        required ></p>
+                    <p>
+                        <label for="login-password">Password: </label>
+                        <input 
+                        id="login-password" 
+                        name="login-password" 
+                        type="password" 
+                        placeholder="Your password"
+                        required >
+                    </p>
+                    <button type="submit" name="login-button">Log in</button>
+                </form>
+                <form action="signup.php" 
+                    method="POST" 
+                    id="signup-form">
+                    <h2>Create an account</h2>
+                    <p>
+                        <label for="signup-firstname">First name: </label>
+                        <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$firstNameLengthInvalid)); ?></p>
+                        <input 
+                        id="signup-firstname" 
+                        name="signup-firstname"
+                        value="<?php getFormValue('signup-firstname'); ?>"
+                        type="text" 
+                        placeholder="Your first name"
+                        required >
+                    </p>
+                    <p>
+                        <label for="signup-lastname">Last name: </label>
+                        <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$lastNameLengthInvalid)); ?></p>
+                        <input 
+                        id="signup-lastname" 
+                        name="signup-lastname" 
+                        value="<?php getFormValue('signup-lastname'); ?>"
+                        type="text"
+                        placeholder="Your last name"
+                        required >
+                    </p>
+                    <p>
+                        <label for="signup-email">Email address: </label>
+                        <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$emailInvalid)); ?></p>
+                        <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$emailAlreadyInUse)); ?></p>
+                        <input 
+                        id="signup-email" 
+                        name="signup-email" 
+                        value="<?php getFormValue('signup-email'); ?>"
+                        type="email" 
+                        placeholder="Your email address" 
+                        required >
+                    </p>
+                    <p>
+                        <label for="signup-email--confirm">Confirm email address: </label>
+                        <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$emailsDoNotMatch)); ?></p>
+                        <input 
+                        id="signup-email--confirm" 
+                        name="signup-email--confirm"
+                        value="<?php getFormValue('signup-email--confirm'); ?>" 
+                        type="email" 
+                        placeholder="Your email address" 
+                        required >
+                    </p>
+                    <p>
+                        <label for="signup-password">Password: </label>
+                        <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$passwordLengthInvalid)); ?></p>
+                        <input 
+                        id="signup-password" 
+                        name="signup-password" 
+                        type="password" 
+                        placeholder="Your password"
+                        required >
+                    </p>
+                    <p>
+                        <label for="signup-password--confirm">Confirm password: </label>
+                        <p class="error-message"><?php echo htmlspecialchars($userlogin->getError(Constants::$passwordsDoNotMatch)); ?></p>
+                        <input 
+                        id="signup-password--confirm" 
+                        name="signup-password--confirm" 
+                        type="password" 
+                        placeholder="Your password"
+                        required >
+                    </p>
+                    <button type="submit" name="signup-button">Sign up</button>
+                </form>
+            </div>
         </div>
     </div>
 </body>
